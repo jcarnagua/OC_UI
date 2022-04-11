b1e76507ca162b25de43d1f72b4a71fc99629eb8
Jon Carnagua <carnaguaj@gmail.com>
2022-04-11 13:52:51 -0400

    Initial username and password logic. No formatting

--- Login-Page/src/app/app.component.ts
+++ Login-Page/src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Login-Page';
+  client: User = new User;
+  hide = true;
+
+  login: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  });
+
+  get usernameInput() {
+    return this.login.get('username');
+  }
+
+  get passwordInput() {
+    return this.login.get('password');
+  }
+
+  getErrorMessage() {
+    return this.usernameInput?.value.hasError('required') ? 'You must enter a value' : 
+      this.usernameInput?.value.hasError('email') ? 'Not a valid email' : 
+      '';
+  }
+
+  setUsername() {
+    this.client.username = this.usernameInput?.value;
+  }
+
+  setPassword() {
+    this.client.password = this.passwordInput?.value;
+  }
+
+  submitCredentials() {
+    this.setUsername();
+    this.setPassword();
+
+    console.log('username: ' + this.client.username);
+    console.log('password: ' + this.client.password);
+  }
+  
 }
--- Login-Page/src/app/app.module.ts
+++ Login-Page/src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
